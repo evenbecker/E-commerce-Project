97c1f1acac8a2842943ecc4d750673110264b352
evenbecker <torettocustom@gmail.com>
2026-03-29 13:05:41 +0200

    typo

--- src/components/basketList/index.js
+++ src/components/basketList/index.js
@@ -1,94 +1,107 @@
-import { Card, IconButton, List, ListItem, Typography } from '@mui/material'
-import React, { useContext } from 'react'
-import { BasketContext } from '../../contexts/basketState'
-import BasketCard from '../basketCard'
-import { red } from '@mui/material/colors'
-import { Link } from 'react-router-dom'
-import { amber } from 'material-ui-colors'
-import { Delete, Devices, Diamond, Man, ShoppingBagSharp, Woman } from '@mui/icons-material'
-import './style.css'
+import { Card, IconButton, List, ListItem, Typography } from "@mui/material";
+import React, { useContext } from "react";
+import { BasketContext } from "../../contexts/basketState";
+import BasketCard from "../basketCard";
+import { red } from "@mui/material/colors";
+import { Link } from "react-router-dom";
+import { amber } from "material-ui-colors";
+import {
+  Delete,
+  Devices,
+  Diamond,
+  Man,
+  ShoppingBagSharp,
+  Woman,
+} from "@mui/icons-material";
+import "./style.css";
 
-const {format} = require('number-currency-format')
+const { format } = require("number-currency-format");
 
 const BasketList = () => {
-
-  const { basketProduct, clearBasket } = useContext(BasketContext)
+  const { basketProduct, clearBasket } = useContext(BasketContext);
 
   const uniqueItems = {};
 
   basketProduct.forEach((basketItem) => {
     const { id } = basketItem;
     if (uniqueItems[id]) {
-      uniqueItems[id].count++
+      uniqueItems[id].count++;
     } else {
       uniqueItems[id] = {
         item: basketItem,
         count: 1,
-      }
+      };
     }
   });
 
   const handleClearAllBasket = () => {
-    clearBasket()
-  }
+    clearBasket();
+  };
 
   if (basketProduct.length !== 0) {
     return (
-      <div className='noEmptyBasket'>
-        <div className='basketList'>
-          <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+      <div className="noEmptyBasket">
+        <div className="basketList">
+          <List sx={{ width: "100%", bgcolor: "background.paper" }}>
             {Object.values(uniqueItems).map(({ item, count }) => (
-              <ListItem
-                key={item}
-                disableGutters
-                className='basketListItem'
-              >
-                <BasketCard
-                  basketItem={item}
-                  basketProductCount={count} />
+              <ListItem key={item} disableGutters className="basketListItem">
+                <BasketCard basketItem={item} basketProductCount={count} />
               </ListItem>
             ))}
           </List>
         </div>
-        <div className='costPrice'>
+        <div className="costPrice">
           <Card>
-            <Typography fontSize={18} fontWeight={600} color={amber['A400']} borderBottom={1} borderColor={'black'} width={200} paddingY={2}>
+            <Typography
+              fontSize={18}
+              fontWeight={600}
+              color={amber["A400"]}
+              borderBottom={1}
+              borderColor={"black"}
+              width={200}
+              paddingY={2}
+            >
               Order Summary
             </Typography>
-            <List className='orderSummarylist'>
+            <List className="orderSummarylist">
               {Object.values(uniqueItems).map(({ item, count }) => (
-                <ListItem
-                  key={item}
-                  disableGutters
-                  className='costPriceItem'
-                >
-                  <Typography className='orderSummaryText' fontSize={14}>
-                    {item.category === "men's clothing" ? <Man />
-                      : item.category === "women's clothing" ? <Woman />
-                        : item.category === "electronics" ?
-                          <Devices />
-                          : <Diamond />
-                    }
+                <ListItem key={item} disableGutters className="costPriceItem">
+                  <Typography className="orderSummaryText" fontSize={14}>
+                    {item.category === "men's clothing" ? (
+                      <Man />
+                    ) : item.category === "women's clothing" ? (
+                      <Woman />
+                    ) : item.category === "electronics" ? (
+                      <Devices />
+                    ) : (
+                      <Diamond />
+                    )}
                     {item.title}
                   </Typography>
                   <Typography fontSize={14}>
-                    {count} items = {format(count * item.price, {
-                      currency: '£',
-                      currencyPosition: 'LEFT',
+                    {count} items ={" "}
+                    {format(count * item.price, {
+                      currency: "€",
+                      currencyPosition: "LEFT",
                       spacing: false,
                     })}
                   </Typography>
                 </ListItem>
               ))}
               <ListItem>
-                <Typography className='totalPrice'>
+                <Typography className="totalPrice">
                   <IconButton
-                    className='deleteAllButton'
-                    onClick={() => handleClearAllBasket()}>
-                    <Delete fontSize='20' /> Delete All Basket
+                    className="deleteAllButton"
+                    onClick={() => handleClearAllBasket()}
+                  >
+                    <Delete fontSize="20" /> Delete All Basket
                   </IconButton>
                   <div>
-                    <span className='totalPriceTitle'>Total Price: </span> £ {Object.values(uniqueItems).reduce((total, { item, count }) => total + (count * item.price), 0)}
+                    <span className="totalPriceTitle">Total Price: </span> €{" "}
+                    {Object.values(uniqueItems).reduce(
+                      (total, { item, count }) => total + count * item.price,
+                      0,
+                    )}
                   </div>
                 </Typography>
               </ListItem>
@@ -96,22 +109,36 @@ const BasketList = () => {
           </Card>
         </div>
       </div>
-    )
+    );
   } else {
     return (
-      <div className='emptyBasket'>
-        <Typography fontSize={18} color={red['A400']} border={1} borderColor={red['A400']} boxShadow={1} padding={5}>
+      <div className="emptyBasket">
+        <Typography
+          fontSize={18}
+          color={red["A400"]}
+          border={1}
+          borderColor={red["A400"]}
+          boxShadow={1}
+          padding={5}
+        >
           There isn't product in your basket!
         </Typography>
         <Link to={"/"}>
-          <Typography backgroundColor={amber['A400']} boxShadow={1} padding={2} color={'black'} fontSize={18} marginTop={2}>
-            <ShoppingBagSharp fontSize='20' />
+          <Typography
+            backgroundColor={amber["A400"]}
+            boxShadow={1}
+            padding={2}
+            color={"black"}
+            fontSize={18}
+            marginTop={2}
+          >
+            <ShoppingBagSharp fontSize="20" />
             SHOPPING NOW
           </Typography>
         </Link>
       </div>
-    )
+    );
   }
-}
+};
 
-export default BasketList
+export default BasketList;
--- src/components/productDetail/index.js
+++ src/components/productDetail/index.js
@@ -1,41 +1,46 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React from 'react'
-import { Badge, Card, CardContent, Typography } from '@mui/material'
-import './style.css'
-import { CircleRounded } from '@mui/icons-material'
-import AddToBasket from '../button'
+import React from "react";
+import { Badge, Card, CardContent, Typography } from "@mui/material";
+import "./style.css";
+import { CircleRounded } from "@mui/icons-material";
+import AddToBasket from "../button";
 
 const ProductDetail = ({ product }) => {
-
-
   return (
-    <div className='productDetailContainer'>
+    <div className="productDetailContainer">
       <Card className="productDetailCard">
-        <CardContent className='detailCard'>
-            <Badge className='badge' color='secondary'>
+        <CardContent className="detailCard">
+          <Badge className="badge" color="secondary">
             {product.category}
           </Badge>
-          <div className='productDetailImage'>
+          <div className="productDetailImage">
             <img src={product.image} />
           </div>
-          <div className='detailCardInfo'>
-           <Typography className="detailProductTitle" gutterBottom fontSize={12} component="div">
-           <CircleRounded/> {product.title}
+          <div className="detailCardInfo">
+            <Typography
+              className="detailProductTitle"
+              gutterBottom
+              fontSize={12}
+              component="div"
+            >
+              <CircleRounded /> {product.title}
             </Typography>
-            <Typography className="detailProductDescription" fontSize={12} component="div">
+            <Typography
+              className="detailProductDescription"
+              fontSize={12}
+              component="div"
+            >
               {product.description}
             </Typography>
-          <Typography variant='body2' fontSize={18}>
-              <div className="detailProductPrice">
-                £ {product.price}
-              </div>
+            <Typography variant="body2" fontSize={18}>
+              <div className="detailProductPrice">€ {product.price}</div>
             </Typography>
-            <AddToBasket item={product}/>
+            <AddToBasket item={product} />
           </div>
         </CardContent>
-     </Card>
+      </Card>
     </div>
-  )
-}
+  );
+};
 
-export default ProductDetail
+export default ProductDetail;
