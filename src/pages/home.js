e80699a4ce7b5d25fb1cb6471837b49f910bb22b
evenbecker <torettocustom@gmail.com>
2026-03-29 12:59:46 +0200

    size logo

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,15 +3,14 @@ import ProductList from "../components/productList";
 import { GlobalState } from "../contexts/globalState";
 
 export const Home = () => {
-   
-  const productsData = useContext(GlobalState)
+  const productsData = useContext(GlobalState);
 
   return (
     <div className="home-container">
       <div className="weAreOpen">
-        <img src="/assets/weareopen.png" alt="open"/>
+        <img src="/assets/ecommerce-logo.jpg" alt="open" />
       </div>
       <ProductList product={productsData} />
-      </div>
+    </div>
   );
-}
+};
